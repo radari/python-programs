706484979964da08e097be3eeee273f2bbda6714
sturrion <sturrion@gmail.com>
2015-04-30 18:35:38 +0200

    testing some things

--- testing_tkinter/hello.py
+++ testing_tkinter/hello.py
@@ -1,10 +1,10 @@
 import tkinter
 # Initialize main windows with title and size
-top = tkinter.Tk()
-top.title("Hello GUI")
-top.minsize(200,30)
+mainwindow = tkinter.Tk()
+mainwindow.title("Hello GUI")
+mainwindow.minsize(200,30)
 # Label widget
-helloLabel = tkinter.Label(top, text = "Hello World!")
+helloLabel = tkinter.Label(mainwindow, text = "Hello World!")
 helloLabel.pack()
 # Start and open the window
-top.mainloop()
+mainwindow.mainloop()
